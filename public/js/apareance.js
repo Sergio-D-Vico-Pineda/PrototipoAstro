a20f519c92fa74c88cda79bc7edb0897083a28ce
Scarpy19_ <nootravez19@gmail.com>
2024-05-26 14:42:30 +0200

    resultContainer

--- public/js/apareance.js
+++ public/js/apareance.js
@@ -1,10 +1,17 @@
 const btnSend = document.getElementById("btnSend");
+
 const chatContainer = document.getElementById("chatContainer");
+const resultContainer = document.getElementById("resultContainer");
+const hr = document.querySelector("hr");
+
 const btnConnect = document.getElementById("btnConnect");
 const iMessage = document.getElementById("message");
 const btnDisconnect = document.getElementById("btnDisconnect");
 const username = document.getElementById("username");
+
 const disconnectContainer = document.getElementById("disconnectContainer");
+const clientUserDisc = document.getElementById("disconnectContainer").querySelector("span");
+
 const header = document.getElementById("header");
 const h1header = document.getElementById("header").querySelector("h1");
 const logo = document.getElementById("logo");
@@ -13,19 +20,24 @@ function fromLoginToChat() {
     header.classList.toggle("flex-col");
     h1header.classList.toggle("text-center");
     h1header.classList.toggle("text-3xl");
-    h1header.classList.toggle("text-xl");
+    h1header.classList.toggle("text-2xl");
     logo.classList.toggle("max-w-96");
-    logo.classList.toggle("max-w-14");
+    logo.classList.toggle("max-w-20");
+    clientUserDisc.innerText = `¡Bienvenido ${username.value}!`;
 }
 
 function changeMessage(active = true) {
     if (active) {
         chatContainer.classList.remove("hidden");
+        resultContainer.classList.remove("hidden");
+        hr.classList.remove("hidden");
         disconnectContainer.classList.remove("hidden");
         disconnectContainer.classList.add("flex");
         form.classList.add("hidden");
     } else {
         chatContainer.classList.add("hidden");
+        resultContainer.classList.add("hidden");
+        hr.classList.add("hidden");
         disconnectContainer.classList.add("hidden");
         disconnectContainer.classList.remove("flex");
         form.classList.remove("hidden");
