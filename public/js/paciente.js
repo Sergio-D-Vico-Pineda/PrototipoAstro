23291a3952d518dc9ce2a767f498e85fc3ac6b84
Scarpy19_ <nootravez19@gmail.com>
2024-06-03 23:30:14 +0200

    idk

---
+++ public/js/paciente.js
@@ -0,0 +1,27 @@
+/* const selUsers = document.getElementById("selUsers"); */
+
+async function listMedic(e) {
+    console.log(e.target.value);
+
+    await fetch("/api/upload", {
+        method: "GET",
+        body: formData,
+        /* headers: {
+            "Content-Type": "multipart/form-data",
+        }, */
+    })
+        .then((res) => {
+            if (res.ok) {
+                alert("File uploaded successfully");
+            }
+        })
+        .catch((err) => {
+            alert(err);
+        });
+}
+
+/* selUsers.addEventListener("change", listMedic) */
+
+export {
+
+}
