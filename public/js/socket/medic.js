352d965812321a1495ba2b3146167f09702fd2ef
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-06-06 19:30:31 +0200

    1 TO 1 CHAT

--- public/js/socket/medic.js
+++ public/js/socket/medic.js
@@ -22,153 +22,153 @@ const inputPassword = document.getElementById("inputPassword");
 let socket;
 
 async function connect(receptorId) {
-    const userId = document.getElementById("userId");
-    try {
-        //socket = io("http://localhost:3000", {
-        socket = io("https://automatic-meme-pv7j9j4j4vqf9pxp-3000.app.github.dev/", {
-            auth: {
-                username: iEmail.value,
-                serverOffset: 1,
-                password: inputPassword.value,
-                usuarioId: userId.value
+   const userId = document.getElementById("userId");
+   try {
+      socket = io("http://192.168.1.138:3000", {
+         //socket = io("https://automatic-meme-pv7j9j4j4vqf9pxp-3000.app.github.dev/", {
+         auth: {
+            username: iEmail.value,
+            serverOffset: 1,
+            password: inputPassword.value,
+            usuarioId: userId.value
+         },
+      });
+
+      socket.on("disconnect", () => {
+         console.log("Desconectado por el servidor");
+      });
+
+      socket.on("error", (err) => {
+         console.log(err);
+      });
+
+      socket.on("connect", async () => {
+         console.log("Conectado al servidor");
+         $receptorIdMedic.innerText = receptorId;
+         console.log('USUARIO EMISOR: ', userId.value, 'USUARIO RECEPTOR: ', receptorId);
+         messages.innerHTML = "";
+         console.log(userId.value, receptorId);
+
+         await fetch(`/api/getmessages`, {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json"
             },
-        });
+            body: JSON.stringify({
+               emisorId: userId.value,
+               receptorId: receptorId
+            })
+         }).then((res) => {
+            return res.json();
+         }).then((data) => {
+            /* console.log(data); */
+            data.forEach((msg) => {
+               console.log(msg);
+            })
+         });
+      });
 
-        socket.on("disconnect", () => {
-            console.log("Desconectado por el servidor");
-        });
+      socket.on('message', (msg, serverOffset, clientUser, origen, destino) => {
+         let item;
+         console.log('ORIGEN: ', origen, 'DESTINO: ', destino);
 
-        socket.on("error", (err) => {
-            console.log(err);
-        });
-
-        socket.on("connect", async () => {
-            console.log("Conectado al servidor");
-            console.log($receptorIdMedic.innerText);
-            $receptorIdMedic.innerText = receptorId;
-            console.log('USUARIO EMISOR: ', userId.value, 'USUARIO RECEPTOR: ', receptorId);
-            messages.innerHTML = "";
-            console.log(userId.value, receptorId);
-
-            await fetch(`/api/getmessages`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    emisorId: userId.value,
-                    receptorId: receptorId
-                })
-            }).then((res) => {
-                return res.json();
-            }).then((data) => {
-                /* console.log(data); */
-                data.forEach((msg) => {
-                    console.log(msg);
-                })
-            });
-        });
-
-        socket.on('message', (msg, serverOffset, clientUser, origen, destino) => {
-            let item;
-
-            console.log('ORIGEN: ', origen, 'DESTINO: ', destino);
-
+         if (destino == userId.value || origen == userId.value || origen == 0) {
             if (origen == userId.value) {
-                item = `<li class="bg-slate-300 border border-black px-5 py-2 flex flex-col rounded-2xl text-end"><span class="text-xl text-wrap break-words">${msg}</span><small class="text-xs text-wrap break-words">${clientUser}</small> </li>`
-                console.log(`${origen} - ${destino}: ${msg}`);
+               item = `<li class="bg-slate-300 border border-black px-5 py-2 flex flex-col rounded-2xl text-end"><span class="text-xl text-wrap break-words">${msg}</span><small class="text-xs text-wrap break-words">${clientUser}</small> </li>`
+               console.log(`${origen} - ${destino}: ${msg}`);
             } else {
-                item = `<li class="bg-slate-400 border border-black px-5 py-2 flex flex-col rounded-2xl"><span class="text-xl text-wrap break-words">${msg}</span><small class="text-xs text-wrap break-words">${clientUser}</small></li>`
-                console.log(`${destino} - ${origen}: ${msg}`);
+               item = `<li class="bg-slate-400 border border-black px-5 py-2 flex flex-col rounded-2xl"><span class="text-xl text-wrap break-words">${msg}</span><small class="text-xs text-wrap break-words">${clientUser}</small></li>`
+               console.log(`${destino} - ${origen}: ${msg}`);
             }
 
             messages.insertAdjacentHTML('beforeend', item)
             socket.auth.serverOffset = serverOffset
 
             chat.scrollTop = chat.scrollHeight
-        })
-    }
-    catch (error) {
-        console.error(error);
-    }
+         }
+      })
+   }
+   catch (error) {
+      console.error(error);
+   }
 };
 
 async function getUsers() {
-    await fetch("/api/pacientes", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-        .then((res) => res.json())
-        .then((data) => {
-            if (data.length > 0) {
-                selUsers.disabled = false;
-                selUsers.innerHTML = "";
-                data.forEach((user) => {
-                    let option = document.createElement("option");
-                    option.value = user.usuarioId;
-                    option.textContent = user.nombre;
-                    selUsers.appendChild(option);
-                });
-            }
-        });
+   await fetch("/api/pacientes", {
+      method: "GET",
+      headers: {
+         "Content-Type": "application/json",
+      },
+   })
+      .then((res) => res.json())
+      .then((data) => {
+         if (data.length > 0) {
+            selUsers.disabled = false;
+            selUsers.innerHTML = "";
+            data.forEach((user) => {
+               let option = document.createElement("option");
+               option.value = user.usuarioId;
+               option.textContent = user.nombre;
+               selUsers.appendChild(option);
+            });
+         }
+      });
 }
 
 async function getChatsMedic() {
-    fetch("/api/chatsmedic")
-        .then((res) => res.json())
-        .then((data) => {
-            if (data.length > 0) {
-                chats.innerHTML = "";
-                data.forEach((chat) => {
-                    let li = document.createElement("li");
-                    li.classList.add(
-                        "bg-slate-300",
-                        "px-5",
-                        "py-2",
-                        "flex",
-                        "items-center",
-                        "justify-around",
-                        "rounded-lg",
-                        "cursor-pointer",
-                        "hover:bg-slate-500",
-                        "transition-colors",
-                    );
-                    li.addEventListener("click", () => {
-                        alert(`Medico ${chat.nombre} y su id es ${chat.usuarioId}`);
-                        chatsContainer.classList.add("hidden");
-                        chatContainer.classList.remove("hidden");
-                        co.connect(chat.usuarioId);
-                    });
+   fetch("/api/chatsmedic")
+      .then((res) => res.json())
+      .then((data) => {
+         if (data.length > 0) {
+            chats.innerHTML = "";
+            data.forEach((chat) => {
+               let li = document.createElement("li");
+               li.classList.add(
+                  "bg-slate-300",
+                  "px-5",
+                  "py-2",
+                  "flex",
+                  "items-center",
+                  "justify-around",
+                  "rounded-lg",
+                  "cursor-pointer",
+                  "hover:bg-slate-500",
+                  "transition-colors",
+               );
+               li.addEventListener("click", () => {
+                  alert(`Medico ${chat.nombre} y su id es ${chat.usuarioId}`);
+                  chatsContainer.classList.add("hidden");
+                  chatContainer.classList.remove("hidden");
+                  co.connect(chat.usuarioId);
+               });
 
-                    let div = document.createElement("div");
-                    div.classList.add("bg-white", "rounded-full", "p-1");
+               let div = document.createElement("div");
+               div.classList.add("bg-white", "rounded-full", "p-1");
 
-                    let p = document.createElement("p");
-                    p.classList.add("text-center", "text-white", "flex-grow");
-                    p.textContent = chat.nombre;
+               let p = document.createElement("p");
+               p.classList.add("text-center", "text-white", "flex-grow");
+               p.textContent = chat.nombre;
 
-                    div.innerHTML = `<svg
+               div.innerHTML = `<svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
                         height="24"
                         viewBox="0 0 256 256"><path fill="currentColor" d="M230.93 220a8 8 0 0 1-6.93 4H32a8 8 0 0 1-6.92-12c15.23-26.33 38.7-45.21 66.09-54.16a72 72 0 1 1 73.66 0c27.39 8.95 50.86 27.83 66.09 54.16a8 8 0 0 1 .01 8"></path></svg>`;
-                    div.innerHTML += `<small class="text-sm">${chat.usuarioId}</small>`
+               div.innerHTML += `<small class="text-sm">${chat.usuarioId}</small>`
 
-                    li.appendChild(div);
-                    li.appendChild(p);
+               li.appendChild(div);
+               li.appendChild(p);
 
-                    chats.append(li);
-                });
-            }
-        });
+               chats.append(li);
+            });
+         }
+      });
 };
 
 function back() {
-    chatsContainer.classList.remove("hidden");
-    chatContainer.classList.add("hidden");
-    socket.disconnect();
+   chatsContainer.classList.remove("hidden");
+   chatContainer.classList.add("hidden");
+   socket.disconnect();
 }
 
 btnBack.addEventListener("click", back);
@@ -176,17 +176,17 @@ btnBack.addEventListener("click", back);
 iMensaje.addEventListener("input", ap.btnState);
 
 btnSend.addEventListener("click", (e) => {
-    e.preventDefault()
+   e.preventDefault()
 
-    if (iMensaje.value == "") {
-        alert("Por favor, ingresa un mensaje");
-        return;
-    }
+   if (iMensaje.value == "") {
+      alert("Por favor, ingresa un mensaje");
+      return;
+   }
 
-    socket.emit("message", iMensaje.value, $receptorIdMedic.innerText);
-    iMensaje.value = "";
+   socket.emit("message", iMensaje.value, $receptorIdMedic.innerText);
+   iMensaje.value = "";
 
-    ap.btnState();
+   ap.btnState();
 })
 
 export { getUsers, getChatsMedic, connect }
--- public/js/socket/paciente.js
+++ public/js/socket/paciente.js
@@ -21,125 +21,127 @@ const btnBack = document.getElementById("btnBack");
 let socket;
 
 async function connect(receptorId) {
-    const userId = document.getElementById("userId");
-    try {
-        socket = io("http://localhost:3000", {
-            auth: {
-                username: iEmail.value,
-                serverOffset: 1,
-                password: inputPassword.value,
-                usuarioId: userId.value
+   const userId = document.getElementById("userId");
+   try {
+      socket = io("http://192.168.1.138:3000", {
+         auth: {
+            username: iEmail.value,
+            serverOffset: 1,
+            password: inputPassword.value,
+            usuarioId: userId.value
+         },
+      });
+
+      socket.on("disconnect", () => {
+         console.log("Desconectado por el servidor");
+      });
+
+      socket.on("error", (err) => {
+         console.log(err);
+      });
+
+      socket.on("connect", async () => {
+         console.log("Conectado al servidor");
+         $receptorId.innerText = receptorId;
+         messages.innerHTML = "";
+         console.log(userId.value, receptorId);
+
+         await fetch(`/api/getmessages`, {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json"
             },
-        });
+            body: JSON.stringify({
+               emisorId: userId.value,
+               receptorId: receptorId
+            })
+         }).then((res) => {
+            return res.json();
+         }).then((data) => {
+            /* console.log(data); */
+            data.forEach((msg) => {
+               console.log(msg);
+            })
+         });
+      });
 
-        socket.on("disconnect", () => {
-            console.log("Desconectado por el servidor");
-        });
+      socket.on('message', (msg, serverOffset, clientUser, origen, destino) => {
+         let item;
 
-        socket.on("error", (err) => {
-            console.log(err);
-        });
+         console.log('ORIGEN: ', origen, 'DESTINO: ', destino);
 
-        socket.on("connect", async () => {
-            console.log("Conectado al servidor");
-            $receptorId.innerText = receptorId;
-            messages.innerHTML = "";
-            console.log(userId.value, receptorId);
-
-            await fetch(`/api/getmessages`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    emisorId: userId.value,
-                    receptorId: receptorId
-                })
-            }).then((res) => {
-                return res.json();
-            }).then((data) => {
-                /* console.log(data); */
-                data.forEach((msg) => {
-                    console.log(msg);
-                })
-            });
-        });
-
-        socket.on('message', (msg, serverOffset, clientUser, origen, destino) => {
-            let item;
-
-            console.log('ORIGEN: ', origen, 'DESTINO: ', destino);
-
+         if (destino == userId.value || origen == userId.value || origen == 0) {
             if (origen == userId.value) {
-                item = `<li class="bg-slate-300 border border-black px-5 py-2 flex flex-col rounded-2xl text-end"><span class="text-xl text-wrap break-words">${msg}</span><small class="text-xs text-wrap break-words">${clientUser}</small> </li>`
-                console.log(`${origen} - ${destino}: ${msg}`);
+               item = `<li class="bg-slate-300 border border-black px-5 py-2 flex flex-col rounded-2xl text-end"><span class="text-xl text-wrap break-words">${msg}</span><small class="text-xs text-wrap break-words">${clientUser}</small> </li>`
+               console.log(`${origen} - ${destino}: ${msg}`);
             } else {
-                item = `<li class="bg-slate-400 border border-black px-5 py-2 flex flex-col rounded-2xl"><span class="text-xl text-wrap break-words">${msg}</span><small class="text-xs text-wrap break-words">${clientUser}</small></li>`
-                console.log(`${destino} - ${origen}: ${msg}`);
+               item = `<li class="bg-slate-400 border border-black px-5 py-2 flex flex-col rounded-2xl"><span class="text-xl text-wrap break-words">${msg}</span><small class="text-xs text-wrap break-words">${clientUser}</small></li>`
+               console.log(`${destino} - ${origen}: ${msg}`);
             }
 
             messages.insertAdjacentHTML('beforeend', item)
             socket.auth.serverOffset = serverOffset
 
             chat.scrollTop = chat.scrollHeight
-        })
-    }
-    catch (error) {
-        console.error(error);
-    }
+         }
+      })
+   }
+   catch (error) {
+      console.error(error);
+   }
 };
 
 async function getResults(userId = 0) {
-    await fetch(`/api/results`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ userId }),
-    }).then((res) => res.json())
-        .then((rs) => {
-            results.innerHTML = "";
-            if (rs.length > 0) {
-                resultTitle.innerHTML = "Todos los resultados";
-                resultSection.classList.add("h-80");
-                results.classList.remove("hidden");
-                for (const row of rs) {
-                    let div = document.createElement("div");
+   await fetch(`/api/results`, {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId }),
+   }).then((res) => res.json())
+      .then((rs) => {
+         results.innerHTML = "";
+         if (rs.length > 0) {
+            resultTitle.innerHTML = "Todos los resultados";
+            resultSection.classList.add("h-80");
+            results.classList.remove("hidden");
+            for (const row of rs) {
+               let div = document.createElement("div");
 
-                    div.classList.add("text-center", "cursor-pointer", "hover:bg-white", "bg-slate-400", "transition-colors", "rounded-xl");
-                    let p = document.createElement("span");
-                    p.classList.add("font-bold");
-                    p.textContent = row.tipoResultado + ' ⇓';
-                    let archivob64 = atob(row.archivo);
+               div.classList.add("text-center", "cursor-pointer", "hover:bg-white", "bg-slate-400", "transition-colors", "rounded-xl");
+               let p = document.createElement("span");
+               p.classList.add("font-bold");
+               p.textContent = row.tipoResultado + ' ⇓';
+               let archivob64 = atob(row.archivo);
 
-                    let obj = document.createElement('object');
-                    obj.classList.add("rounded-xl", "w-full");
-                    obj.type = 'application/pdf';
-                    obj.data = 'data:application/pdf;base64,' + row.archivo;
+               let obj = document.createElement('object');
+               obj.classList.add("rounded-xl", "w-full");
+               obj.type = 'application/pdf';
+               obj.data = 'data:application/pdf;base64,' + row.archivo;
 
-                    let link = document.createElement('a');
-                    link.classList.add("overflow-hidden");
-                    link.download = row.descripcion + '.pdf';
-                    link.href = 'data:application/octet-stream;base64,' + row.archivo;
-                    link.title = 'File Size: ' + Math.round(archivob64.length / 1024) + ' KB - PDF Version: ' + archivob64.match(/^.PDF-([0-9.]+)/)[1];
+               let link = document.createElement('a');
+               link.classList.add("overflow-hidden");
+               link.download = row.descripcion + '.pdf';
+               link.href = 'data:application/octet-stream;base64,' + row.archivo;
+               link.title = 'File Size: ' + Math.round(archivob64.length / 1024) + ' KB - PDF Version: ' + RegExp(/^.PDF-([0-9.]+)/).exec(archivob64)[1];
 
-                    link.append(obj)
-                    link.append(p)
-                    div.append(link)
-                    results.append(div);
-                }
-            } else if (rs.length === 0) {
-                resultTitle.innerHTML = "No hay resultados";
-                resultSection.classList.remove("h-80");
-                results.classList.add("hidden");
+               link.append(obj)
+               link.append(p)
+               div.append(link)
+               results.append(div);
             }
-        })
+         } else if (rs.length === 0) {
+            resultTitle.innerHTML = "No hay resultados";
+            resultSection.classList.remove("h-80");
+            results.classList.add("hidden");
+         }
+      })
 }
 
 function back() {
-    chatsContainer.classList.remove("hidden");
-    chatContainer.classList.add("hidden");
-    socket.disconnect();
+   chatsContainer.classList.remove("hidden");
+   chatContainer.classList.add("hidden");
+   socket.disconnect();
 }
 
 btnBack.addEventListener("click", back);
@@ -147,17 +149,17 @@ btnBack.addEventListener("click", back);
 iMensaje.addEventListener("input", ap.btnState);
 
 btnSend.addEventListener("click", (e) => {
-    e.preventDefault()
+   e.preventDefault()
 
-    if (iMensaje.value == "") {
-        alert("Por favor, ingresa un mensaje");
-        return;
-    }
+   if (iMensaje.value == "") {
+      alert("Por favor, ingresa un mensaje");
+      return;
+   }
 
-    socket.emit("message", iMensaje.value, $receptorId.value);
-    iMensaje.value = "";
+   socket.emit("message", iMensaje.value, $receptorId.innerText);
+   iMensaje.value = "";
 
-    ap.btnState();
+   ap.btnState();
 })
 
 export { connect, getResults };
